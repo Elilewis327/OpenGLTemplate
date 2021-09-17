1db034f1fc0641f1089dd7903968504b5a60df53
Eli Lewis <jan@Elis-MacBook-Air.local>
2021-09-17 16:13:22 -0400

    fml

--- build/glad/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jan/Bin/GameofLife/glad
-# Build directory: /Users/jan/Bin/GameofLife/build/glad
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
