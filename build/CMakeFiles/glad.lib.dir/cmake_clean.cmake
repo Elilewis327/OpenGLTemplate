ef90fe165f6102a035a1bc417da690a5e23d5634
Eli Lewis <jan@Elis-MacBook-Air.local>
2021-09-17 16:04:55 -0400

    first

---
+++ build/CMakeFiles/glad.lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glad.lib.dir/glad.c.o"
+  "CMakeFiles/glad.lib.dir/glad.c.o.d"
+  "glad.lib"
+  "glad.lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/glad.lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/glad/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jan/Bin/GameofLife/glad
+# Build directory: /Users/jan/Bin/GameofLife/build/glad
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
